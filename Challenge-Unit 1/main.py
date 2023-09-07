4d590ca012d689f1c84a3b16d1100439135402d6
asunm1330222206504 <144369409+asunm1330222206504@users.noreply.github.com>
2023-09-07 21:59:45 +0530

    Add files via upload

---
+++ Challenge-Unit 1/main.py
@@ -0,0 +1,27 @@
+def CheckLeap(Year):  
+  
+  if((Year % 400 == 0) or  
+     (Year % 100 != 0) and  
+     (Year % 4 == 0)):   
+    print("Given Year is a leap Year");  
+  
+  else:  
+    print ("Given Year is not a leap Year")  
+
+Year = int(input("Enter the number: "))  
+
+CheckLeap(Year)long int multiplyNumbers(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+
+long int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
